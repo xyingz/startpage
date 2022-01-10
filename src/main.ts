3679dd1b2e99cf1a8a5162e2f3c5831eb6f40fe7
xyingz <190313747@qq.com>
2022-01-10 11:09:10 +0800

    feat(dependencies): 添加 quasar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
 import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import quasarLang from 'quasar/lang/zh-CN';
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
+
 import App from './App.vue';
 import router from './router/index';
 import store, { key } from './store/index';
 import './styles/index.scss';
 
-createApp(App).use(router).use(store, key).mount('#app');
+const app = createApp(App);
+
+app
+  .use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    lang: quasarLang
+    /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+  })
+  .use(router)
+  .use(store, key)
+  .mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import eslintPlugin from 'vite-plugin-eslint';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({ template: { transformAssetUrls } }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    }),
     eslintPlugin({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.ts']
     })
