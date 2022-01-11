c884d53b3c0e62ec437862c1a5677d2f502a2c55
xyingz <190313747@qq.com>
2022-01-11 14:09:59 +0800

    fix(merge): 更新工具内容

--- src/store/data/tools.ts
+++ src/store/data/tools.ts
@@ -1,105 +1,96 @@
 import { Tool } from '@/typings/tool';
+import { uuid } from '@/utils/common';
 
 const tools: Array<Tool> = [
   {
-    id: '1',
-    url: 'https://www.google.com',
-    comment: 'Google'
-  },
-  {
-    id: '2',
+    id: uuid(12),
     url: 'https://pan.baidu.com',
     comment: '百度网盘'
   },
   {
-    id: '3',
-    url: 'https://github.com',
-    comment: 'GitHub'
-  },
-  {
-    id: '4',
-    url: 'https://www.jd.com',
-    comment: '京东'
-  },
-  {
-    id: '5',
-    url: 'https://www.bilibili.com',
-    comment: 'Bilibili'
-  },
-  {
-    id: '6',
-    url: 'https://www.weibo.com',
-    comment: '微博'
-  },
-  {
-    id: '7',
-    url: 'https://www.zhihu.com',
-    comment: '知乎'
-  },
-  {
-    id: '8',
-    url: 'https://web.wechat.com/?lang=zh_CN',
-    comment: '微信网页版'
-  },
-  {
-    id: '9',
-    url: 'https://www.taobao.com',
-    comment: '淘宝'
-  },
-  {
-    id: '10',
-    url: 'https://www.npmjs.com',
-    comment: 'npm'
-  },
-  {
-    id: '11',
-    url: 'https://juejin.im',
-    comment: '掘金'
-  },
-  {
-    id: '12',
-    url: 'https://www.csdn.net',
-    comment: 'CSDN'
-  },
-  {
-    id: '13',
-    url: 'https://gitee.com',
-    comment: '码云Gitee'
-  },
-  {
-    id: '14',
-    url: 'https://stackoverflow.com',
-    comment: 'StackOverflow'
-  },
-  {
-    id: '15',
-    url: 'https://segmentfault.com',
-    comment: 'SegmentFault'
-  },
-  {
-    id: '16',
-    url: 'https://www.jianshu.com',
-    comment: '简书'
-  },
-  {
-    id: '17',
+    id: uuid(12),
     url: 'https://mail.qq.com',
     comment: 'QQ邮箱'
   },
   {
-    id: '18',
+    id: uuid(12),
+    url: 'https://mail.163.com/',
+    comment: '163邮箱'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.jd.com',
+    comment: '京东'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.taobao.com',
+    comment: '淘宝'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.bilibili.com',
+    comment: 'Bilibili'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.weibo.com',
+    comment: '微博'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.zhihu.com',
+    comment: '知乎'
+  },
+  {
+    id: uuid(12),
+    url: 'https://web.wechat.com/?lang=zh_CN',
+    comment: '微信网页版'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.npmjs.com',
+    comment: 'npm'
+  },
+  {
+    id: uuid(12),
+    url: 'https://juejin.im',
+    comment: '掘金'
+  },
+  {
+    id: uuid(12),
+    url: 'https://segmentfault.com',
+    comment: 'SegmentFault'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.jianshu.com',
+    comment: '简书'
+  },
+  {
+    id: uuid(12),
+    url: 'https://www.csdn.net',
+    comment: 'CSDN'
+  },
+  {
+    id: uuid(12),
+    url: 'https://github.com',
+    comment: 'GitHub'
+  },
+  {
+    id: uuid(12),
+    url: 'https://gitee.com',
+    comment: '码云Gitee'
+  },
+  {
+    id: uuid(12),
+    url: 'https://stackoverflow.com',
+    comment: 'StackOverflow'
+  },
+  {
+    id: uuid(12),
     url: 'https://greasyfork.org/zh-CN/scripts',
     comment: '油猴脚本'
-  },
-  {
-    id: '19',
-    url: 'https://stackshare.io',
-    comment: 'StackShare'
-  },
-  {
-    id: '20',
-    url: 'https://xiaopangying.com',
-    comment: '小胖鹰'
   }
 ];
 
