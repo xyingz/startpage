39a0522bff70d969fc25f4e884d6cb6db4ca5d8d
xyingz <190313747@qq.com>
2022-01-12 15:56:48 +0800

    style(dependencies): 删除不必要的注释

--- .eslintrc.js
+++ .eslintrc.js
@@ -61,52 +61,6 @@ module.exports = {
 
     'no-undef': 'off',
 
-    // 【必须】缩进 2 个空格
-    // indent: [
-    //   "error",
-    //   2,
-    //   { SwitchCase: 1, ObjectExpression: 1, offsetTernaryExpressions: true }
-    // ],
-    // 【必须】换行符风格
-    // "linebreak-style": [
-    //   "error",
-    //   process.platform === "win32" ? "windows" : "unix"
-    // ],
-    // 【必须】单引号
-    // quotes: ["error", "single", { avoidEscape: true }],
-    // 【必须】始终适用分号
-    // semi: ["error", "always"],
-    // 【必须】驼峰命名
-    // camelcase: ["error", { properties: "never" }],
-    // 【必须】对象短写
-    // "object-shorthand": ["error", "always"],
-    // 【必须】数组不要保持两侧间距
-    // "array-bracket-spacing": [
-    //   "error",
-    //   "never",
-    //   {
-    //     objectsInArrays: false,
-    //     arraysInArrays: false
-    //   }
-    // ],
-    // 【必须】全等
-    // eqeqeq: ["error", "always"],
-    // 【必须】数组回调方法强制执行 return 语句
-    // "array-callback-return": "error",
-    // 【必须】箭头函数中箭头保持两侧间距
-    // "arrow-spacing": "error",
-    // 【必须】视 var 为块作用域
-    // "block-scoped-var": "error",
-    // 【必须】同行块级内容保持两侧间距
-    // "block-spacing": "error",
-    // 【必须】大括号样式，允许单行
-    // "brace-style": ["error", "1tbs", { allowSingleLine: true }],
-
-    // 关闭 ts 声明 any 的报错
-    // "@typescript-eslint/no-explicit-any": "off",
-    // 关闭 ts 中函数缺少返回类型的报错
-    // "@typescript-eslint/explicit-module-boundary-types": "off",
-
     // 关于 vue 更多规则可以查阅 https://eslint.vuejs.org/rules
     'vue/max-attributes-per-line': 'off',
     'vue/require-default-prop': 'off',
