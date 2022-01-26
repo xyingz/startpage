f8cbb51c7a2a31c582c768dac4990af54954bcba
xyingz <190313747@qq.com>
2022-01-26 17:15:05 +0800

    fix: delete useless code

--- src/api/lunar/index.ts
+++ src/api/lunar/index.ts
@@ -1,15 +1,15 @@
-import { get } from '@/utils/http/requests';
-import { lunarUrl } from '../url';
+// import { get } from '@/utils/http/requests';
+// import { lunarUrl } from '../url';
 
-export default async function getLunar() {
-  const [err, weather] = await get<{ results: { chinese_calendar: Lunar[] } }>(
-    lunarUrl
-  );
+// export default async function getLunar() {
+//   const [err, weather] = await get<{ results: { chinese_calendar: Lunar[] } }>(
+//     lunarUrl
+//   );
 
-  if (err || !weather) {
-    if (!import.meta.env.PROD) console.error(err);
-    return '日历暂时不可用';
-  }
+//   if (err || !weather) {
+//     if (!import.meta.env.PROD) console.error(err);
+//     return '日历暂时不可用';
+//   }
 
-  return weather.results.chinese_calendar;
-}
+//   return weather.results.chinese_calendar;
+// }
