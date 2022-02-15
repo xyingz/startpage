c54e0139e877489578836d7d0ab70db79cbcbd7e
xyingz <190313747@qq.com>
2022-02-15 18:10:47 +0800

    test: update test

--- test/unit/Test.spec.ts
+++ test/unit/Test.spec.ts
@@ -1,11 +1,11 @@
 import { shallowMount } from '@vue/test-utils';
-import App from '@/App.vue';
+import App from '@/pages/404.vue';
 
 describe('Test', () => {
   const wrapper = shallowMount(App);
 
   it('load compoent', () => {
     const html = wrapper.text();
-    expect(html).toContain('XiaoPangYing');
+    expect(html).toContain('页面不存在');
   });
 });
