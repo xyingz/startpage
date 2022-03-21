c918ea7f8dfcd31ba462aff55d9490d3e46cf07e
xyingz <190313747@qq.com>
2022-03-21 11:36:49 +0800

    feat: 便签列表添加空时的内容

--- src/components/note/NotesListDialog.vue
+++ src/components/note/NotesListDialog.vue
@@ -4,47 +4,64 @@
       style="min-width: 200px; max-width: 600px"
       class="q-pa-md q-gutter-md"
     >
-      <template v-for="note in notes" :key="note.id">
-        <q-item
-          class="rounded-borders no-padding overflow-hidden"
-          clickable
-          @dblclick="() => note.isClose && openNote(note)"
-        >
-          <q-item-section
-            style="
-              max-height: 10rem;
-              background-color: darkgrey;
-              justify-content: start;
-            "
+      <template v-if="notes.length">
+        <template v-for="note in notes" :key="note.id">
+          <q-item
+            class="rounded-borders no-padding overflow-hidden"
+            clickable
+            @dblclick="() => note.isClose && openNote(note)"
           >
-            <div
-              class="full-width absolute-top"
-              :style="{ height: '3px', backgroundColor: note.backgroundColor }"
-            />
-            <!-- eslint-disable vue/no-v-html -->
-            <p class="q-pa-sm" v-html="note.content" />
-            <div v-if="!note.isClose" class="x-note-open-mark" />
+            <q-item-section
+              style="
+                max-height: 10rem;
+                background-color: darkgrey;
+                justify-content: start;
+              "
+            >
+              <div
+                class="full-width absolute-top"
+                :style="{
+                  height: '3px',
+                  backgroundColor: note.backgroundColor
+                }"
+              />
+              <!-- eslint-disable vue/no-v-html -->
+              <p class="q-pa-sm" v-html="note.content" />
+              <div v-if="!note.isClose" class="x-note-open-mark" />
 
-            <!-- 右键菜单 -->
-            <q-menu context-menu>
-              <q-list dense>
-                <q-item clickable>
-                  <q-item-section>
-                    删除该标签
-                    <q-popup-proxy auto-close>
-                      <q-btn
-                        round
-                        dense
-                        color="positive"
-                        icon="done"
-                        @click="() => deleteNote(note)"
-                      />
-                      <q-btn round dense color="negative" icon="close" />
-                    </q-popup-proxy>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-menu>
+              <!-- 右键菜单 -->
+              <q-menu context-menu>
+                <q-list dense>
+                  <q-item clickable>
+                    <q-item-section>
+                      删除该标签
+                      <q-popup-proxy auto-close>
+                        <q-btn
+                          round
+                          dense
+                          color="positive"
+                          icon="done"
+                          @click="() => deleteNote(note)"
+                        />
+                        <q-btn round dense color="negative" icon="close" />
+                      </q-popup-proxy>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-item-section>
+          </q-item>
+        </template>
+      </template>
+      <template v-else>
+        <q-item>
+          <q-item-section>
+            <q-item-label>
+              <q-icon name="note" />
+            </q-item-label>
+            <q-item-label>
+              <q-item-label> 暂无笔记 </q-item-label>
+            </q-item-label>
           </q-item-section>
         </q-item>
       </template>
